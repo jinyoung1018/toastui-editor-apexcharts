c59c4407cb9297f83d4c2bfb728a9516cab1e104
jinyoung1018 <jywsb1816@gmail.com>
2023-04-04 16:21:57 +0900

    text editor and apexcharts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,125 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div id="editor"></div>
+    <div id="chart">
+      <apexchart type="rangeBar" height="350" :options="chartOptions" :series="series" ></apexchart>
+    </div>
   </div>
 </template>
+
+<script>
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
+import VueApexCharts from 'vue3-apexcharts';
+import moment from "moment";
+
+
+export default {
+
+
+  data: function () {
+    return {
+      editor: null,
+      series: [
+        {
+          data: [
+            {
+              x: 'Analysis',
+              y: [
+                new Date('2019-02-27').getTime(),
+                new Date('2019-03-04').getTime()
+              ],
+              fillColor: '#008FFB'
+            },
+            {
+              x: 'Design',
+              y: [
+                new Date('2019-03-04').getTime(),
+                new Date('2019-03-08').getTime()
+              ],
+              fillColor: '#00E396'
+            },
+            {
+              x: 'Coding',
+              y: [
+                new Date('2019-03-07').getTime(),
+                new Date('2019-03-10').getTime()
+              ],
+              fillColor: '#775DD0'
+            },
+            {
+              x: 'Testing',
+              y: [
+                new Date('2019-03-08').getTime(),
+                new Date('2019-03-12').getTime()
+              ],
+              fillColor: '#FEB019'
+            },
+            {
+              x: 'Deployment',
+              y: [
+                new Date('2019-03-12').getTime(),
+                new Date('2019-03-17').getTime()
+              ],
+              fillColor: '#FF4560'
+            }
+          ]
+        }
+      ],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'rangeBar'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            distributed: true,
+            dataLabels: {
+              hideOverflowingLabels: false
+            }
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: function (val, opts) {
+            var label = opts.w.globals.labels[opts.dataPointIndex]
+            var a = moment(val[0])
+            var b = moment(val[1])
+            var diff = b.diff(a, 'days')
+            return label + ': ' + diff + (diff > 1 ? ' days' : ' day')
+          },
+          style: {
+                colors: ['#f3f4f5', '#fff']
+              }
+        },
+        xaxis: {
+              type: 'datetime'
+            },
+            yaxis: {
+              show: false
+            },
+            grid: {
+              row: {
+                colors: ['#f3f4f5', '#fff'],
+                opacity: 1
+              }
+            }
+
+      },
+      
+
+    }
+  },
+  mounted() {
+        this.editor = new Editor({
+          el: document.querySelector('#editor'),
+          height: '500px',
+          initialEditType: 'markdown',
+          previewStyle: 'vertical'
+        });
+
+      },
+
+}
+</script>
